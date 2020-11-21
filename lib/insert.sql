63647b8a0df85f99a0975a30e49fa5ea4cd04309
RTBlanco <ronnytoribio1@hotmail.com>
2020-11-20 23:37:10 -0500

    commit tree 11fc03a3243c259e08a699b8de5579f131ba979b

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "black", "happy", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 22, "M", "white", "happy", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "black", "happy", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 33, "M", "white", "happy", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 40, "F", "brown", "happy", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 10, "M", "brown", "happy", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 23, "F", "white", "happy", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 12, "M", "black", "angry", 0);
